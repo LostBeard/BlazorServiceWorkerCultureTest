8b3aef2d8dc8148102d968457e674f5bff9001a0
Todd Tanner <todd@spawndev.com>
2025-04-02 14:55:11 -0400

    Modified notification click handling

--- BlazorServiceWorkerCultureTest/ServiceWorker.cs
+++ BlazorServiceWorkerCultureTest/ServiceWorker.cs
@@ -1,4 +1,5 @@
-﻿using SpawnDev.BlazorJS;
+﻿using Microsoft.AspNetCore.Components;
+using SpawnDev.BlazorJS;
 using SpawnDev.BlazorJS.JSObjects;
 using SpawnDev.BlazorJS.WebWorkers;
 
@@ -18,7 +19,7 @@ namespace BlazorServiceWorkerCultureTest
             // demo payload for example
             // normally info is extracted from the event's PushMessageData at e.Data
             // Example read push message data as a Json object - var jsonObject = e.Data.Json<DataObjectType>();
-            var payload = new
+            var payload = new Payload
             {
                 Message = "Waether Alert.",
                 Url = "/weather",
@@ -35,21 +36,44 @@ namespace BlazorServiceWorkerCultureTest
         {
             try
             {
+                // handle the selected action
                 var actionChosen = e.Action;
                 JS.Log("ServiceWorker_OnNotificationClickAsync", actionChosen);
                 switch (e.Action)
                 {
                     case "open":
                         {
+                            // get the notification
                             using var notification = e.Notification;
+
+                            // access the notification data set in the push event handler
+                            var payload = notification.DataAs<Payload>();
+                            
+
+                            // close the notification
                             notification.Close();
+
+
+                            // work with windows the way you would in Javascript
                             var windowClients = await ServiceWorkerThis!.Clients.MatchAll(new ClientsMatchAllOptions { Type = "window", IncludeUncontrolled = true });
-                            // for an example of communicating with open windows, use WebWorkerService to call JS.Log in all open windows
+                            // bring an existing window to focus, open a new window, etc
+
+                            // OR
+
+                            // WebWorkerService provides a way to directly call any service running in any in winow running your app
+                            // for an example use WebWorkerService to call JS.Log in all open windows
                             var windows = WebWorkerService.Instances.Where(o => o.Info.Scope == GlobalScope.Window).ToList();
                             foreach (var window in windows)
                             {
                                 // this will use the BlazorJSRuntime service in the window's scope to call js.Log
-                                await window.Run<BlazorJSRuntime>(js => js.Log("notification was clicked"));
+                                // any service can be accessed in any running instance of your app in any scope (windows, workers, etc)
+                                await window.Run<BlazorJSRuntime>(js => js.Log("message from service worker: notification was clicked"));
+                                // 
+
+                                // navigate this window to payload.Url
+                                await window.Run<NavigationManager>(navigationManager => navigationManager.NavigateTo(payload.Url.TrimStart('/'), false, false));
+
+                                //await window.Run<BlazorJSRuntime>(js => js.CallVoid("alert", "notification was clicked"));
                             }
                         }
                         break;
@@ -57,7 +81,8 @@ namespace BlazorServiceWorkerCultureTest
             }
             catch (Exception ex)
             {
-                JS.Log();
+                JS.Log("ServiceWorker_OnNotificationClickAsync error", ex.Message);
+                JS.Log(ex.StackTrace ?? "");
             }
         }
         protected override async Task<Response> ServiceWorker_OnFetchAsync(FetchEvent e)
